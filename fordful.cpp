dca3b5b9383d3bd2ea7e06d6459a12a451289ea2
poulomi-biswas <94776150+poulomi-biswas@users.noreply.github.com>
2023-09-08 12:34:59 +0530

    Update fordful.cpp

--- fordful.cpp
+++ fordful.cpp
@@ -37,9 +37,11 @@ bool bfs(int** rGraph, int s, int t, int* parent, int rank)
                     parent[v] = u;
                     visited[v] = true;
                     path_flow=1;
+                    MPI_Send(visited, V, MPI_INT, 1, 0, MPI_COMM_WORLD);
                     MPI_Send(&path_flow, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
                 }
                 else {
+                    MPI_Recv(visited, V, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                     MPI_Recv(&path_flow, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                 }
             }
